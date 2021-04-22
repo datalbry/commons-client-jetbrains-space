c0ebf32419b8a80d050b4da59f654f2cfe5a98ae
Zeljko Bekcic <bekciczeljko@gmail.com>
2021-04-22 19:52:36 +0200

    Correctly implementing pagination iterator

---
+++ client/src/main/kotlin/io/datalbry/jetbrains/space/client/PaginationIterator.kt
@@ -0,0 +1,37 @@
+package io.datalbry.jetbrains.space.client
+
+import space.jetbrains.api.runtime.Batch
+import space.jetbrains.api.runtime.BatchInfo
+
+class PaginationIterator<T, S>(
+    private val fetchNextBatch: (BatchInfo) -> Batch<T>,
+    private val transformer: (T) -> S
+) : Iterator<S> {
+    private var batchInfo = BatchInfo("0", 100)
+    private var currentBatch: Batch<T> = fetchNextBatch(batchInfo)
+    private var currentBatchElements = currentBatch.data.map(transformer).iterator()
+
+    private fun loadNextBatch() {
+        val currentBatchTmp = fetchNextBatch(batchInfo)
+        if (currentBatchTmp.next != "") {
+            currentBatch = currentBatchTmp
+            currentBatchElements = currentBatch.data.map(transformer).iterator()
+            batchInfo = BatchInfo(currentBatch.next, 100)
+        }
+    }
+
+    override fun hasNext(): Boolean {
+        if (!currentBatchElements.hasNext()) {
+            loadNextBatch()
+        }
+        return currentBatchElements.hasNext()
+    }
+
+    override fun next(): S {
+        if (!currentBatchElements.hasNext()) {
+            loadNextBatch()
+        }
+
+        return currentBatchElements.next()
+    }
+}
